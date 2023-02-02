cadd526ca8cb6f2edc3c642ae45c1b61999de564
MoeyR <122491063+MoeyR@users.noreply.github.com>
2023-02-01 20:54:18 -0500

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -69,19 +69,18 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Moyu, but you can call me by my English name Moey!");
         System.out.println();
         
-        System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("My study ambitions:");
+        System.out.println("-- Be more diligent on coding, and stay hungry!")
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Coding");
+        System.out.println("-- Financing");
+        System.out.println("-- Reading and Workout");
+        System.out.println("-- Walking my dog");
 
         System.out.println();
         
